0dfa3700b81ea32740c4da85e62d06053083e679
jriordan-akrivia <john.riordan@akriviahealth.com>
2026-01-07 17:17:41 +0000

    setting up postgres

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-dc down
+docker-compose down --remove-orphans
 yes | docker system prune
 yes | docker network prune
 yes | docker volume prune
 docker-compose up -d --build
-docker exec -it next yes | pnpm install
+#docker-compose exec -it next 'yes | pnpm install'
 docker cp next:app/node_modules/ ./node_modules
